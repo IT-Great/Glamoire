6fb591100139ffdbf0803d00abde549988bbed8a
ITGCS <helmi.satria@great.sch.id>
2025-07-02 08:23:25 +0700

    help, update quantity keranjang

--- resources/views/user/component/help.blade.php
+++ resources/views/user/component/help.blade.php
@@ -1,26 +1,24 @@
 @extends('user.layouts.master')
 
 @section('content')
-<section class="md:px-20 lg:px-24 xl:px-24 2xl:px-48 pt-2 py-2">
-  <div class="container-fluid px-0 px-md-3">
-    <div class="shadow-sm border border-black rounded-sm py-2 py-md-3 my-2 my-md-3">
-      <div class="d-flex gap-1 pl-3">
-        <a href="/home" class="text-[10px] md:text-[12px] lg:text-[12px] xl:text-[14px]">Beranda</a>
-        <p class="text-[10px] md:text-[12px] lg:text-[12px] xl:text-[14px]"> > </p>
-        <a href="#" class="text-black text-[10px] md:text-[12px] lg:text-[12px] xl:text-[14px]">Pusat Bantuan</a>
-      </div>
-    </div>
-  </div>
+    <section class="md:px-20 lg:px-24 xl:px-24 2xl:px-48 py-2 mb-4">
+        <!-- Existing breadcrumb code -->
 
-  <div class="container-fluid px-0 px-md-3">
-    <nav class="tabbable">
-      <div class="nav nav-tabs border-secondary mb-4">
-          <a class="nav-item nav-link active text-[10px] md:text-[12px] lg:text-[14px] xl:text-[16px]" data-bs-toggle="tab" href="#akun">Akun</a>
-          <a class="nav-item nav-link text-[10px] md:text-[12px] lg:text-[14px] xl:text-[16px]" data-bs-toggle="tab" href="#order">Pembelian</a>
-          <a class="nav-item nav-link text-[10px] md:text-[12px] lg:text-[14px] xl:text-[16px]" data-bs-toggle="tab" href="#payment">Pembayaran</a>
-          <a class="nav-item nav-link text-[10px] md:text-[12px] lg:text-[14px] xl:text-[16px]" data-bs-toggle="tab" href="#shipping">Pengiriman</a>
-      </div>
-    </nav>
+        <div class="container-fluid">
+            <nav class="tabbable">
+                <div class="nav nav-tabs border-secondary mb-4">
+                    @php
+                        $categories = $faqsByCategory->keys();
+                    @endphp
+
+                    @foreach ($categories as $index => $category)
+                        <a class="nav-item nav-link {{ $index === 0 ? 'active' : '' }} text-[10px] md:text-[12px] lg:text-[14px] xl:text-[16px]"
+                            data-bs-toggle="tab" href="#{{ Str::slug($category) }}">
+                            {{ ucfirst($category) }}
+                        </a>
+                    @endforeach
+                </div>
+            </nav>
 
             <div class="tab-content">
                 @foreach ($categories as $index => $category)
@@ -48,6 +46,7 @@
                             </div>
                         </div>
                     </div>
+                @endforeach
             </div>
             
         </div>
