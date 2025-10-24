1445587d2b7837ebc4585e0fb62b1ae772af35d7
ITGCS <helmi.satria@great.sch.id>
2025-10-24 10:02:53 +0700

    webhook

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BiteshipController;
 
-Route::post('/callback-glamoire-with-biteship', [BiteshipController::class, 'callback']);
+Route::get('/callback-glamoire-with-biteship', [BiteshipController::class, 'callback']);
