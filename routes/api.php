b9d101849b9ac628ca8d200a3003ae838740cd08
Sean Alden <c14210052@john.petra.ac.id>
2026-08-24 09:51:11 +0700

    add xendit controller

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\XenditController;
 use App\Http\Controllers\BiteshipController;
 
 Route::post('/callback-glamoire-with-biteship', [BiteshipController::class, 'callback']);
